02f180580bbc1b73ae0b2385feb42da8ccc0709a
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-05 19:08:36 -0600

    added method fire_upon and fired_upon? passed final 2 tests

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,16 +1,28 @@
 class Cell
-    attr_reader :coordinate, :ship
+    attr_reader :coordinate, 
+                :ship, 
+                :fired_upon
 
     def initialize(coordinate)
         @coordinate = coordinate
         @ship = nil
+        @fired_upon = false
     end
 
-    def empty?
+    def empty? #tells if cell is empty
         @ship.nil?
     end
 
-    def place_ship(ship)
+    def place_ship(ship) # places ship in cell
         @ship = ship
+    end
+
+    def fired_upon? # if fired upon it will come up false
+        @fired_upon
+    end
+
+    def fire_upon
+        @fired_upon = true #marks cell as fired upon
+        @ship.hit if @ship # reduce ship health by 1
     end
 end
