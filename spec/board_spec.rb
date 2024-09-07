4f876007a31e472d7ffd198266bc8c012e8ce172
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-09-07 11:38:47 -0500

    feat: added ships to the spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,6 +3,8 @@ require './spec/spec_helper'
 RSpec.describe Board do
     before (:each) do   
         @board = Board.new
+        @cruiser = Ship.new("Cruiser", 3)
+        @submarine = Ship.new("Submarine", 2)
     end
     
     it '#makes a board' do
