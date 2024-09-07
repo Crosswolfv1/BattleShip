180a018ac166184ec607dce8f2667a87018ed547
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-09-07 14:43:29 -0500

    refactor: fixed formatting

--- lib/board.rb
+++ lib/board.rb
@@ -8,10 +8,10 @@ class Board
 
     def create_cell# we need to create the board and that requires create_cells 4x4
         cells = {} # makes a Hash
-            ("A".."D").each do |row|# this goes A->D in a row <--->
+        ("A".."D").each do |row|# this goes A->D in a row <--->
             (1..4).each do |col|# this goes 1->4 in a column ^----v
-            coordinate = "#{row}#{col}"# we will give what the coordinate is to return our cell
-            cells[coordinate] = Cell.new(coordinate)
+                coordinate = "#{row}#{col}"# we will give what the coordinate is to return our cell
+                cells[coordinate] = Cell.new(coordinate)
             end
         end
         cells
