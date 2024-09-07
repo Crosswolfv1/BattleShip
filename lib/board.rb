f5cc42707cd497c6e706e3dd4cfce99bf2321cad
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-07 10:16:05 -0600

    created board class and added cells

---
+++ lib/board.rb
@@ -0,0 +1,21 @@
+class Board
+
+    attr_reader :cells
+
+    def initialize# no argument needed
+    @cells = create_cell #create the cell
+    end
+
+    def create_cell# we need to create the board and that requires create_cells 4x4
+        cells = {} # makes a Hash
+            ("A".."D").each do |row|# this goes A->D in a row <--->
+            (1..4).each do |col|# this goes 1->4 in a column ^----v
+            coordinate = "#{row}#{col}"# we will give what the coordinate is to return our cell
+            cells[coordinate] = Cell.new(coordinate)
+            end
+        end
+        cells
+    end
+
+    
+end
