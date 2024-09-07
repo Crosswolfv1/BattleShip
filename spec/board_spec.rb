3a26242c15aa0f02b42105ad5506fb539cbc75c1
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-09-07 11:46:46 -0500

    test: created tests for valid_placement?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -30,4 +30,28 @@ RSpec.describe Board do
         expect(@board.valid_coordinate?("D5")).to eq false
     end
 
+    describe '#valid_placement?' do
+        it 'provided num of coords equal to ship length' do
+        @board.valid_placement?(@cruiser, ["A1", "A2"])
+        @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
+        end
+
+        it 'coords are consecutive' do
+            @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+            @board.valid_placement?(@submarine, ["A1", "C1"])
+            @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
+            @board.valid_placement?(@submarine, ["C1", "B1"])
+        end
+
+        it 'coords are NOT diagonal' do
+            @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+            @board.valid_placement?(@submarine, ["C2", "D3"])
+        end
+
+        it 'can be valid' do
+            @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
+            @board.valid_placement?(@submarine, ["A1", "A2"])
+        end
+    end
+
 end
