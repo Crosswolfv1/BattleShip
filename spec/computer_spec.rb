337579ad1ed40902445d0e10bc935ebc297781c7
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-08 10:44:50 -0600

    added generate coordinates tests pass

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Computer do
         it 'exists' do
             expect(@computer).to be_an_instance_of(Computer)
         end
+
+        it '#generate_coordinates' do
+            expect(@computer.generate_coordinates(3).count).to eq(3)
+            expect(@computer.generate_coordinates(2).count).to eq(2)
+        end
 end
