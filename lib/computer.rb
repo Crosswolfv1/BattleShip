431e3d938943064d4eb4ceb502a479f017117b77
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-08 10:46:05 -0600

    Added generate coordinates method

--- lib/computer.rb
+++ lib/computer.rb
@@ -3,4 +3,36 @@ class Computer
     def initialize
         @board = Board.new
     end
+
+    def generate_coordinates(length)
+        possible_rows = ("A".."D").to_a.sample
+        possible_cols = (1..4).to_a.sample
+        coordinates = []
+
+        while coordinates.length != length
+            coordinates.clear
+            orientation = ["horizontal", "vertical"].sample
+            if orientation =="horizontal"
+                length.times do |i| 
+                    col = possible_cols
+                    if col + i <= 4
+                        coordinates << "#{possible_rows}#{col + i}"
+                    end
+                end
+            else orientation == "vertical"
+                length.times do |o|
+                    row = possible_rows
+                    if (row.ord + o).chr <= "D"
+                        coordinates << "#{(row.ord + o).chr}#{possible_cols}"
+                    end
+                end
+            end
+        end
+        coordinates
+    end
+
+
+    def place_ships(ship)
+        @board.valid_placement?(ship, coordinates)
+    end
 end
