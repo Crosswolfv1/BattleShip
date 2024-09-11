8cbc08e406238a9e03f9c0ffb270b5dd950c8bff
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-09-11 09:46:26 -0500

    Test: modified test to pass the new code

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,109 +1,93 @@
 require 'spec_helper'
 
-RSpec.describe Game do
-    before(:each) do
-        @computer_board = Board.new
-        @player_board = Board.new
-        @computer = Computer.new(@computer_board)
-        @game = Game.new(@player_board, @computer_board, @computer)
-        @player_cruiser = Ship.new("Cruiser", 3)
-        @player_submarine = Ship.new("Submarine", 2)
-        @computer_cruiser = Ship.new("Cruiser", 3)
-        @computer_submarine = Ship.new("Submarine", 2)
+describe Game do  # Use `describe` here for the Game class
+  before(:each) do
+    allow($stdout).to receive(:puts)
+    allow_any_instance_of(Game).to receive(:gets).and_return("A1 A2 A3", "B1 B2")
+    @game = Game.new
+    @game.setup_phase
+    @player_cruiser = Ship.new("Cruiser", 3)
+    @player_submarine = Ship.new("Submarine", 2)
+    @computer_cruiser = Ship.new("Cruiser", 3)
+    @computer_submarine = Ship.new("Submarine", 2)
+  end
+
+  describe 'starts a game' do 
+    it 'exists' do
+      expect(@game).to be_an_instance_of(Game)
+    end
+  end
+
+  describe 'player places ships' do
+    it 'prompts the player and places ships on the board' do
+      expect(@game.player_board.cells["A1"].ship).to be_an_instance_of(Ship)
+      expect(@game.player_board.cells["A2"].ship).to be_an_instance_of(Ship)
+      expect(@game.player_board.cells["A3"].ship).to be_an_instance_of(Ship)
+      expect(@game.player_board.cells["B1"].ship).to be_an_instance_of(Ship)
+      expect(@game.player_board.cells["B2"].ship).to be_an_instance_of(Ship)
+    end
+  end
+
+  describe 'computer places ships' do 
+    it 'can place ships' do
+      @game.place_computer_ships
+      placed_cells = @game.computer_board.cells.values.select { |cell| !cell.empty? }
+
+      placed_cells.each do |cell|
+        expect(cell.ship).to be_an_instance_of(Ship)
+      end
+    end
+  end
+
+  describe 'player turn' do
+    it "fires upon a valid coordinate on the computer's board" do
+      placed_cells = @game.computer_board.cells.values.select { |cell| !cell.empty? }
+      allow_any_instance_of(Game).to receive(:gets).and_return(placed_cells[0].coordinate)
+      @game.player_turn
+
+      expect(@game.computer_board.cells[placed_cells[0].coordinate].fired_upon?).to be true
+      expect(@game.computer_board.cells[placed_cells[0].coordinate].render).to eq("H")
+    end
+  end
+
+  describe 'computer turn' do
+    it "fires at a valid random coordinate on the player's board" do
+      @game.player_board.place(@player_cruiser, ["A1", "A2", "A3"])
+      initial_fired_upon = @game.player_board.cells.values.count { |cell| cell.fired_upon? }
+      @game.computer_turn
+      final_fired_upon = @game.player_board.cells.values.count { |cell| cell.fired_upon? }
+
+      expect(final_fired_upon).to eq(initial_fired_upon + 1)
+    end
+  end
+
+  describe 'game_over?' do
+    it "ends the game when all player's ships are sunk" do
+      @game.player_board.place(@player_cruiser, ["A1", "A2", "A3"])
+      @game.player_board.place(@player_submarine, ["B1", "B2"])
+
+      @game.player_board.cells["A1"].fire_upon
+      @game.player_board.cells["A2"].fire_upon
+      @game.player_board.cells["A3"].fire_upon
+      @game.player_board.cells["B1"].fire_upon
+      @game.player_board.cells["B2"].fire_upon
+
+      expect(@game.all_ships_sunk?(@game.player_board)).to be true
+      expect(@game.game_over?).to be true
     end
 
-    describe 'starts a game' do 
-        it 'exists' do
-            expect(@game).to be_an_instance_of(Game)
-            expect(@game.player_board).to eq(@player_board)
-            expect(@game.computer_board).to eq(@computer_board)
-            expect(@game.computer).to eq(@computer)
-        end
+    it "ends the game when all computer's ships are sunk" do
+      @game.computer_board.place(@computer_cruiser, ["A1", "A2", "A3"])
+      @game.computer_board.place(@computer_submarine, ["B1", "B2"])
+
+      placed_cells = @game.computer_board.cells.values.select { |cell| !cell.empty? }
+
+      placed_cells.each do |cell|
+        cell.fire_upon
+      end
+
+      expect(@game.all_ships_sunk?(@game.computer_board)).to be true
+      expect(@game.game_over?).to be true
     end
-
-    describe 'player places ships' do
-        it 'prompts the player and places ships on the board' do
-            allow($stdout).to receive(:puts)
-            allow_any_instance_of(Game).to receive(:gets).and_return("A1 A2 A3", "B1 B2")
-
-            @game.place_player_ships #allows for custom ship values
-          
-            expect(@player_board.cells["A1"].ship).to be_an_instance_of(Ship)
-            expect(@player_board.cells["A2"].ship).to be_an_instance_of(Ship)
-            expect(@player_board.cells["A3"].ship).to be_an_instance_of(Ship)
-            expect(@player_board.cells["B1"].ship).to be_an_instance_of(Ship)
-            expect(@player_board.cells["B2"].ship).to be_an_instance_of(Ship)
-        end
-    end
-
-    describe 'computer places ships' do 
-        it 'can place ships' do
-            allow($stdout).to receive(:puts)
-            @game.place_computer_ships
-            
-            placed_cells = @computer_board.cells.values.select { |cell| !cell.empty? }
-    
-            placed_cells.each do |cell|
-            expect(cell.ship).to be_an_instance_of(Ship)
-            end
-        end
-    end
-    
-    describe 'player turn' do
-        it "fires upon a valid coordinate on the computer's board" do
-            allow($stdout).to receive(:puts)
-
-            @computer_board.place(@computer_cruiser, ["A1", "A2", "A3"])
-            expect(@computer_board.cells["A1"].fired_upon?).to be false
-            
-            allow_any_instance_of(Game).to receive(:gets).and_return("A1")
-            @game.player_turn("A1")
-      
-            expect(@computer_board.cells["A1"].fired_upon?).to be true
-            expect(@computer_board.cells["A1"].render).to eq("H")
-        end
-    end
-
-    describe '#computer_turn' do
-        it "fires at a valid random coordinate on the player's board" do
-            allow($stdout).to receive(:puts)
-            @player_board.place(@player_cruiser, ["A1", "A2", "A3"])
-  
-            initial_fired_upon = @player_board.cells.values.count { |cell| cell.fired_upon? }
-            @game.computer_turn
-  
-            final_fired_upon = @player_board.cells.values.count { |cell| cell.fired_upon? }
-            expect(final_fired_upon).to eq(initial_fired_upon + 1)
-        end
-    end
-
-    describe "#game_over?" do
-        it "ends the game when all of player's ships are sunk" do
-            @player_board.place(@player_cruiser, ["A1", "A2", "A3"])
-            @player_board.place(@player_submarine, ["B1", "B2"])
-
-            @player_board.cells["A1"].fire_upon
-            @player_board.cells["A2"].fire_upon
-            @player_board.cells["A3"].fire_upon
-            @player_board.cells["B1"].fire_upon
-            @player_board.cells["B2"].fire_upon
-
-            expect(@game.all_ships_sunk?(@player_board)).to be true
-            expect(@game.game_over?).to be true
-        end
-
-        it "ends the game when all of computer's ships are sunk" do
-            @computer_board.place(@computer_cruiser, ["A1", "A2", "A3"])
-            @computer_board.place(@computer_submarine, ["B1", "B2"])
-  
-            @computer_board.cells["A1"].fire_upon
-            @computer_board.cells["A2"].fire_upon
-            @computer_board.cells["A3"].fire_upon
-            @computer_board.cells["B1"].fire_upon
-            @computer_board.cells["B2"].fire_upon
-  
-            expect(@game.all_ships_sunk?(@computer_board)).to be true
-            expect(@game.game_over?).to be true
-        end
-    end
+  end
 end
